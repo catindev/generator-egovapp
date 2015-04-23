151ba5fd6a9d4026d27f119e58b419a39dabc3da
Владимир Тицкий <Vladimir.Titskiy@inessoft.kz>
2015-04-23 10:45:49 +0600

    upd

--- app/index.js
+++ app/index.js
@@ -37,11 +37,11 @@ module.exports = yeoman.generators.Base.extend({
   var fs = require('fs'),
     xml2js = require('xml2js');
     var parser = new xml2js.Parser();
-    parser.addListener('end', function(result) {
+    parser.addListener('end', function(pom) {
         console.dir(result);
         console.log('Done.');
     });
-    fs.readFile('./pom.xml', function(err, data) {
+    fs.readFile('./webapp/pom.xml', function(err, data) {
         parser.parseString(data);
     });
   },
