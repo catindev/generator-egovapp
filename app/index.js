d3bfe7a691c94beca66eb88d1b6c91a1497948dc
Владимир Тицкий <Vladimir.Titskiy@inessoft.kz>
2015-04-27 10:53:19 +0600

    upd

--- app/index.js
+++ app/index.js
@@ -6,6 +6,8 @@ var yosay = require('yosay');
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
+    this.folder =  process.cwd().split('\\');
+    this.folder =  this.folder[this.folder.length-1];
   },
 
   prompting: function () {
@@ -25,6 +27,19 @@ module.exports = yeoman.generators.Base.extend({
       this.copy('_gulpfile.js', webapp + 'gulpfile.js'); 
       this.copy('_.bowerrc', webapp + '.bowerrc'); 
       this.copy('_.npmignore', webapp + '.npmignore');              
+    },
+    makelink: function() {
+      var rimraf = require('rimraf');
+      var self = this;
+      console.log(__dirname.replace("/"+this.folder, ""));
+      rimraf('webapp/src/main/webapp/node_modules/egov-pep-frontend/build', function () {
+        self.spawnCommand(
+          'ln', 
+            [
+              '-s', 
+              'webapp/src/main/webapp/node_modules/egov-pep-frontend/build ../egov-pep-frontend/src/main/resources/META-INF/resources/build'
+        ]);
+      });      
     }
   }
 });
